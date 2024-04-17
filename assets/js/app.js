96a2b42197cffa719ce6bf52419cf93c37804007
MassimoRomano <romano93m@gmail.com>
2024-04-17 16:05:54 +0200

    add json_content

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+console.log('test');
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    }
+}).mount('#app');
